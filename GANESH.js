d6657f4727a12acb8e1e55aedc4e85df61579d0f
22KShah-hub-art <120753197+22KShah-hub-art@users.noreply.github.com>
2024-01-31 21:48:45 +0000

    Add files via upload

---
+++ GANESH.js
@@ -0,0 +1,34 @@
+function back()
+{
+    window.location="index.html";
+}
+
+status1="";
+
+function preload()
+{
+    img=loadImage("Ganesh.JPG");
+}
+
+function setup()
+{
+   canvas=createCanvas(640,420);
+   canvas.center();
+   objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+   document.getElementById("status").innerHTML="status:Detecting Objects";
+}
+
+function modelLoaded()
+{
+status1=true;
+console.log("model Loaded");
+objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+if(error){
+    console.log(error);
+}
+console.log(results);
+}
